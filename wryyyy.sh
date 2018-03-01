7943727b690563893ff4af9a4ba0f443fa74c7c9
Kiri <32393884+KiriKira@users.noreply.github.com>
2018-03-01 00:48:23 -0800

    don't know what happened.

--- wryyyy.sh
+++ wryyyy.sh
@@ -20,7 +20,6 @@ bash <(curl -L -s https://install.direct/go.sh)
 curl -s -o caddy_install.sh  https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/caddy_install.sh && chmod +x caddy_install.sh && bash caddy_install.sh install http.forwardproxy
 
 curl -s -o /etc/v2ray/config.json https://raw.githubusercontent.com/KiriKira/vTemplate/master/websocket%2BCaddy%2BTLS\(use%20path\)/config_server.json
-curl -s -o wryyy.pyo https://github.com/KiriKira/scripts/raw/master/wryyy.pyo
 
 Get_IP
 python wryyy.pyo $ip
