ad57268b386e6db8449ad4a887bdb2c9559ec821
KiriKira <32393884+KiriKira@users.noreply.github.com>
2018-03-24 22:32:35 +0800

    update

--- caddy-systemd.sh
+++ caddy-systemd.sh
@@ -2,14 +2,22 @@
 # Program:
 #	Simple shell script fot install caddy and configure the systemd-service.
 # Author: Kiri 
+# Usage: bash caddy-systemd.sh http.filemanager,http.forwardproxy
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 install_caddy(){
-    curl https://getcaddy.com | bash -s personal ${plugins}
+    wget -qO- https://getcaddy.com | bash -s personal ${plugins}
 }
 
 service_caddy(){
-    
+    wget -O /etc/systemd/system/caddy.service https://raw.githubusercontent.com/KiriKira/scripts/master/caddy.service
+    mkdir -p /etc/ssl/caddy
+    mkdir /etc/caddy
+    touch /etc/caddy/Caddyfile
+    systemctl enable caddy.service
 }
 
+plugins=$1
+install_caddy
+service_caddy
